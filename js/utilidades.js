c8ff1e30852ecab18f3c7693adae9620aac85fbf
MateoVb13 <mateo.villalobos@cecar.edu.co>
2023-10-19 20:58:46 -0500

    juas juas

---
+++ js/utilidades.js
@@ -0,0 +1,40 @@
+function crearNodo(tipoNodo) {
+    var nodo = document.createElement(tipoNodo);
+    return nodo;
+
+
+}
+
+function crearNodoTexto(tipoNodo, texto) {
+    var nodo = document.createElement(tipoNodo);
+    var textoNodo = document.createTextNode(texto);
+    nodo.appendChild(textoNodo);
+    return nodo;
+}
+
+function adicionarNodoBody(nodo) {
+    document.body.appendChild(nodo);
+}
+
+function adicionarNodoContenedor(contenedor, nodo) {
+    contenedor.appendChild(nodo);
+}
+
+function crearNodoA(texto, href) {
+    var nodo = crearNodoTexto("a", texto);
+    nodo.href = href
+    return nodo;
+}
+
+function crearTabla(filas, columnas) {
+    var tabla = crearNodo("table");
+    for (var i = 0; i < filas; i++) {
+        var fila = crearNodo("tr");
+        for (var j = 0; j < columnas; j++) {
+            var celda = crearNodo("td");
+            fila.appendChild(celda);
+        }
+        tabla.appendChild(fila);
+    }
+    return tabla;
+}
